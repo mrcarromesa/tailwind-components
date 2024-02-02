3af46e64b7dc86615b7115e6f48dda5933fe706a
Rodolfo <mrcarromesa@gmail.com>
2024-02-02 07:37:43 -0300

    tests

--- tests/modal.spec.ts
+++ tests/modal.spec.ts
@@ -1,19 +1,13 @@
 import { test, expect } from '@playwright/test'
 
 test('Open Modal', async ({ page }) => {
-  await page.goto(
-    'http://127.0.0.1:6006/iframe.html?args=&id=components-modal--default'
-  )
-
-  await expect(
-    page.getByRole('button', { name: 'Open Modal' })
-  ).toBeInViewport()
+  await page.goto('?id=components-modal--default&viewMode=story')
+  await expect(page.getByRole('button', { name: 'Open Modal' })).toBeVisible()
   await page.getByRole('button', { name: 'Open Modal' }).click()
-  await expect(page.getByRole('dialog')).toBeAttached()
-
-  // TODO:
-
+  await expect(page.getByRole('dialog')).toBeVisible()
   await page.locator('[id="__-modal-areas"]').getByRole('button').click()
 
-  await expect(page.getByRole('dialog')).not.toBeAttached()
+  await page.waitForTimeout(500)
+
+  await expect(page.getByRole('dialog')).not.toBeVisible()
 })
